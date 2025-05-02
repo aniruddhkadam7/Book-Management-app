376492906cbb1d231a406a204799a920e2515a34
Aniruddh kadam <theaniruddhkadam@gmail.com>
2025-05-02 17:32:15 +0530

    updated

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -12,7 +12,7 @@ import java.util.Set;
 @Table(name = "books",
         indexes = {
                 @Index(name = "idx_book_title", columnList = "title"),
-                @Index(name = "idx_book_isbn", columnList = "isbn", unique = true) //dd
+                @Index(name = "idx_book_isbn", columnList = "isbn", unique = true) //dddd
         })
 @Getter
 @Setter
