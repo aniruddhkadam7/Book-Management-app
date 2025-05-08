c393082ac901691243a9ecb7e1d908061e3845e5
Aniruddh kadam <theaniruddhkadam@gmail.com>
2025-05-08 10:34:46 +0530

    updated

--- src/main/java/org/example/repository/ReviewRepository.java
+++ src/main/java/org/example/repository/ReviewRepository.java
@@ -18,7 +18,7 @@ public interface ReviewRepository extends BaseRepository<Review, Long> {
     /**
      * Filters reviews by approval status.
      *
-     * @param approved true for approved reviews, false for pending gggdddddddffuu
+     * @param approved true for approved reviews, false for pending gggdddddddffuuii
      */
     List<Review> findByApproved(boolean approved);
 
