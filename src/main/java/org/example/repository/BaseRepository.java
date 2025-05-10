d1e383b536826eb5002c5d0a127ccf720cc255bf
Aniruddh kadam <theaniruddhkadam@gmail.com>
2025-05-10 17:16:04 +0530

    updated

--- src/main/java/org/example/repository/BaseRepository.java
+++ src/main/java/org/example/repository/BaseRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.NoRepositoryBean;
 import java.util.Optional;
 
 /**
- * Base repository for all entities extending BaseEntity. mmmkk
+ * Base repository for all entities extending BaseEntity. mmmkkkk
  * Provides common soft-delete operations.
  *
  * @param <T>  Entity type (e.g., Author, Book)
